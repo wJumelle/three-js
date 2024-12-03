e17a322e02a29ae9e328c404de3b68ea0858ac35
wJumelle <wjumelle@gmail.com>
2024-12-03 23:34:33 +0100

    [04][VID1] Animation avec génération procédurale 2/2

--- decouverte/script_procedural.js
+++ decouverte/script_procedural.js
@@ -27,16 +27,17 @@ const renderer = new THREE.WebGLRenderer({canvas})
 
 // On setup une clock pour l'animation
 const clock = new THREE.Clock()
+let t = 0
 
 // On anime la propriété rotation de notre cube
 loop()
 
 function loop() {
   // On calcule le delta entre deux frame
-  const dt = clock.getDelta()
+  t += clock.getDelta()
 
-  // Et on fait tourner sur l'axe Y notre mesh en fonction de ce delta
-  mesh.rotateY(0.1 * dt)
+  // On souhaite animer nos vertex en fonction de la variation t pour chaque frame
+  animateGeometry(geometry, t)
 
   // On lance une instruction pour effectuer un premier rendu en précisant la scène que l'on souhaite afficher et la caméra
   renderer.render(scene, camera)
@@ -105,4 +106,61 @@ function computeGeometry() {
   geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
 
   return geometry
+}
+
+/**
+ * animateGeometry - Fonction qui va permettre d'animer notre modèle 3D générer procéduralement
+ * @param {*} geometry - Geométrie du modèle 3D
+ * @param {*} progress - Progression du temps
+ */
+function animateGeometry(geometry, progress) {
+  // On génère une surface de 4 sur 4
+  const space = 4
+
+  // Avec un découpage de 100 points par 100 points
+  const nb = 100
+
+  // Avec une variation d'amplitude de 0.1
+  const amp = 0.1
+
+  // Et une période que l'on fait varier en fonction du temps
+  const fre = 0.8 + Math.cos(progress) / 2
+
+  const pi2 = Math.PI*2
+
+  // On ajoute un déphasage
+  const phase = progress
+
+  // On parcourt chaque point de notre surface, en largeur (i) et en longueur (j)
+  let k = 0
+  for(let i = 0; i < nb; i++) {
+    for(let j = 0; j < nb; j++) {
+      // Pour chaque vertex on va créer la position x, avec 0 comme centre de notre surface
+      const x = i * (space/nb) - space/2
+
+      // On fait la même chose pour la position z
+      const z = j * (space/nb) - space/2
+
+      // On fait varier la hauteur de notre surface (son amplitude) via une sinusoude baséee sur l'axe x
+      // On ajoute une variation de l'amplitude par l'axe z
+      const y = amp * (Math.cos(x * pi2 * fre + phase) + Math.sin(z * pi2 * fre + phase))
+
+      // Pour chacun des points on va venir écraser la position Y par la nouvelle position
+      geometry.attributes.position.setY(k, y)
+
+      // On calcule l'intensité de la couleur des pixels en fonction de l'amplitude
+      const intensity = (y/amp) / 2 + 0.3
+
+      // Et on écrase l'ancienne couleur qui va varier en fonction de la hauteur du point
+      // On calcule la couleur RGB pour chaque point
+      geometry.attributes.color.setX(k, j / nb * intensity)
+      geometry.attributes.color.setZ(k, i / nb * intensity)
+
+      k++
+    }
+  }
+
+  // On indique au moteur que la géométrie a été mise à jour et doit être rafraichie dans la VRAM
+  geometry.attributes.position.needsUpdate = true
+  geometry.attributes.color.needsUpdate = true
 }
